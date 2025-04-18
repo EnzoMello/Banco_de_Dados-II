82fd31fbbbd1d9d86ebc9c7e1853bcb99a50032c
EnzoMello <enzomelo333@gmail.com>
2025-04-18 17:42:38 -0300

    feat: adiciona mais questões feitas da lista 2 do BD Hotel

--- Hotel_Geral.sql
+++ Hotel_Geral.sql
@@ -56,6 +56,13 @@ VALUES ('RITA', '2000-5-29');
 INSERT INTO FUNCIONARIO (NOME, DT_NASC)
 VALUES ('CLEITON', '1980-3-20');
 
+-- CREATE TABLE RESERVA
+-- (COD_RES INT SERIAL PRIMARY KEY, NUM_AP INT, COD_HOSP INT, COD_FUNC INT, DATA_RES DATE, DT_ENT, DT_SAI, 
+-- FOREIGN KEY (NUM_AP) REFERENCES APARTAMENTO(NUM_AP),
+-- FOREIGN KEY (COD_HOSP) REFERENCES HOSPEDE(COD_HOSP),
+-- FOREIGN KEY (COD_FUNC) REFERENCES FUNCIONARIO(COD_FUNC),
+-- )
+
 ALTER TABLE HOSPEDE 
 ADD COLUMN NAC VARCHAR (100);
 UPDATE HOSPEDE
@@ -133,17 +140,41 @@ GROUP BY H.NOME, HO.NUM
 ORDER BY QUANTIDADE DESC
 
 -- 7. Categoria cujo nome tenha comprimento superior a 15 caracteres.
+SELECT NOME FROM CATEGORIA WHERE LENGTH(NOME)>15;
+
 -- 8. Número dos apartamentos ocupados no ano de 2017 com o respectivo nome da sua
 -- categoria.
+SELECT AP.NUM_AP, C.NOME FROM APARTAMENTO AP
+JOIN CATEGORIA C ON AP.COD_CAT = C.COD_CAT
+JOIN HOSPEDAGEM HO ON AP.NUM_AP = HO.NUM_AP
+WHERE HO.DT_ENT >= '2017-01-01' AND HO.DT_SAIDA <= '2017-12-31'
+GROUP BY AP.NUM_AP, C.NOME
+
 -- 9. Título do livro, nome da editora que o publicou e a descrição do assunto.
+-- Não há como fazer.
+
 -- 10. Crie a tabela funcionário com as atributos: cod_func, nome, dt_nascimento e salário.
 -- Depois disso, acrescente o cod_func como chave estrangeira nas tabelas hospedagem e
 -- reserva.
+-- Questão já foi feita até a parte de adicionar cod_func como chave estrangeira na tabela hospedagem, falta criar a tabela reserva.
+-- Tabela reserva criada e comentada lá em cima
+
+
 -- 11. Mostre o nome e o salário de cada funcionário. Extraordinariamente, cada funcionário
 -- receberá um acréscimo neste salário de 10 reais para cada hospedagem realizada.
+SELECT NOME,SALARIO+COUNT(COD_HOSPEDA)*10 
+FROM FUNCIONARIO F LEFT JOIN HOSPEDAGEM H
+ON F.COD_FUNC=H.COD_FUNC
+GROUP BY NOME,SALARIO
+
 -- 12. Listagem das categorias cadastradas e para aquelas que possuem apartamentos, relacionar
 -- também o número do apartamento, ordenada pelo nome da categoria e pelo número do
 -- apartamento.
+SELECT C.NOME, AP.NUM_AP FROM CATEGORIA C
+LEFT JOIN APARTAMENTO AP ON C.COD_CAT = AP.COD_CAT
+ORDER BY C.NOME, AP.NUM_AP
+
+
 -- 13. Listagem das categorias cadastradas e para aquelas que possuem apartamentos, relacionar
 -- também o número do apartamento, ordenada pelo nome da categoria e pelo número do
 -- apartamento. Para aquelas que não possuem apartamentos associados, escrever "não possui
@@ -153,13 +184,49 @@ FROM CATEGORIA C LEFT JOIN APTO A ON C.COD_CAT=A.COD_CAT
 
 -- 14. O nome dos funcionário que atenderam o João (hospedando ou reservando) ou que
 -- hospedaram ou reservaram apartamentos da categoria luxo.
+SELECT DISTINCT F.NOME FROM FUNCIONARIO F
+JOIN HOSPEDAGEM HO ON F.COD_FUNC = HO.COD_FUNC
+JOIN APARTAMENTO AP ON HO.NUM_AP = AP.NUM_AP
+JOIN CATEGORIA C ON C.COD_CAT = AP.COD_CAT
+JOIN HOSPEDE H ON HO.COD_HOSP = H.COD_HOSP
+WHERE H.NOME ILIKE 'João%' OR C.NOME ILIKE '%luxo%'
+
 -- 15. O código das hospedagens realizadas pelo hóspede mais velho que se hospedou no
 -- apartamento mais caro.
+SELECT COD_HOSPEDA FROM HOSPEDAGEM WHERE 
+COD_HOSP IN (SELECT COD_HOSP FROM HOSPEDE WHERE 
+DT_NASC IN (SELECT MIN(DT_NASC) FROM HOSPEDE WHERE 
+COD_HOSP IN (SELECT COD_HOSP FROM HOSPEDAGEM WHERE
+NUM IN (SELECT NUM FROM APTO WHERE
+COD_CAT IN (SELECT COD_CAT FROM CATEGORIA WHERE
+VALOR_DIA IN (SELECT MAX(VALOR_DIA) FROM CATEGORIA))))))
+
 -- 16. Sem usar subquery, o nome dos hóspedes que nasceram na mesma data do hóspede de
 -- código 2.
--- 17. O nome do hóspede mais velho que se hospedou na categoria mais cara mo ano de 2017.
+SELECT H2.NOME FROM HOSPEDE H1,HOSPEDE H2 WHERE 
+H1.COD_HOSP<>H2.COD_HOSP AND H1.COD_HOSP=7 AND H1.DT_NASC=H2.DT_NASC
+
+-- 17. O nome do hóspede mais velho que se hospedou na categoria mais cara no ano de 2017.
+SELECT H.NOME FROM HOSPEDE H 
+JOIN HOSPEDAGEM HO ON H.COD_HOSP = HO.COD_HOSP 
+JOIN APARTAMENTO AP ON AP.NUM_AP = HO.NUM_AP
+JOIN CATEGORIA C ON C.COD_CAT = AP.COD_CAT
+WHERE H.DT_NASC IN (SELECT MIN(DT_NASC) FROM HOSPEDE)
+AND C.PRECO = (SELECT MAX(PRECO) FROM CATEGORIA) 
+AND HO.DT_ENT >= '2017-01-01' AND HO.DT_SAIDA <= '2017-12-31'
+
+
 -- 18. O nome das categorias que foram reservadas pela Maria ou que foram ocupadas pelo João
 -- quando ele foi atendido pelo Joaquim.
+SELECT C.NOME FROM CATEGORIA C
+JOIN APARTAMENTO AP ON C.COD_CAT = AP.COD_CAT
+JOIN HOSPEDAGEM HO ON HO.NUM_AP = AP.NUM_AP
+JOIN FUNCIONARIO F ON HO.COD_FUNC = F.COD_FUNC
+JOIN HOSPEDE H ON HO.COD_HOSP = H.COD_HOSP
+WHERE H.NOME ILIKE 'João%' AND F.NOME ILIKE 'Joaquim%' OR H.NOME ILIKE 'Maria%'
+AND F.NOME ILIKE 'Joaquim%'
+
+
 -- 19. O nome e a data de nascimento dos funcionários, além do valor de diária mais cara
 -- reservado por cada um deles.
 -- 20. A quantidade de apartamentos ocupados por cada um dos hóspedes (mostrar o nome)
@@ -188,3 +255,7 @@ SELECT NOME AS CATEGORIA ORDER BY CATEGORIA ASC
 -- Nome de todas as categorias e, quando possível, os números do respectivo apartamento
 SELECT NOME, NUM FROM APTO A RIGHT JOIN CATEGORIA C
 ON A.COD_CAT = C.COD_CAT
+
+
+-- ****************** APRENDIZADO AULA 12/04/205 ******************
+-- ---> VISÕES DE BD:
